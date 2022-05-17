704a81d594bf7079bdc07804dd6d05f7b6108349
Ryan-Phillips Iserhills Cornelio <ryan.cornelio@gmail.com>
2022-05-17 19:16:42 +0200

    style update

--- src/gameData.ts
+++ src/gameData.ts
@@ -7,11 +7,11 @@ export const questions: Story[] = [
     text: 'You wake up in your room...',
     options: {
       left:{
-        text: 'Go outside',
+        text: 'Go outside...',
         goTo: 2
       },
       right: {
-        text: 'Brush your teeth',
+        text: 'Brush your teeth...',
         goTo: 3
       }
     }, 
@@ -22,10 +22,10 @@ export const questions: Story[] = [
         options: 
         {
           left: {
-            text: 'Go to the park',
+            text: 'Go to the park...',
             goTo: 4
           }, right: {
-            text: 'Go to the store',
+            text: 'Go to the store...',
             goTo: 5
           } 
         }, 
@@ -35,25 +35,51 @@ export const questions: Story[] = [
   options: {
     left:{
       text: 'Go for a jog...',
-      goTo: 2
+      goTo: 6
     },
     right: {
       text: 'Watch tv...',
-      goTo: 3
+      goTo: 7
     }
   }, 
     }, {
         id: 4,
-      text: 'You see alot of happy people in the enjoying the good weather...',
+      text: 'You see alot of happy people ouside...',
       options: {
         left:{
-          text: 'Buy a hotdog',
-          goTo: 2
+          text: 'Buy a hotdog...',
+          goTo: 5
         },
         right: {
-          text: 'Call a friend',
-          goTo: 3
+          text: 'Call a friend...',
+          goTo: 5
         }
       }, 
-        },
+        }, {
+          id: 5,
+        text: 'You realize you are low on money...',
+        options: {
+          left:{
+            text: 'Just buy a drink...',
+            goTo: 6
+          },
+          right: {
+            text: 'Go back home...',
+            goTo: 1
+          }
+        }, 
+          }, {
+            id: 6,
+          text: 'Fresh cold ice tea...',
+          options: {
+            left:{
+              text: 'Go party...',
+              goTo: 6
+            },
+            right: {
+              text: 'Go back home...',
+              goTo: 1
+            }
+          }, 
+            },
   ]
